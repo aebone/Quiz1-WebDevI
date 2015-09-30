8411b87e8f726008e80e857cc9cc2d2787946d07
Aline Ebone <ini.ebone@gmail.com>
2015-09-30 16:33:13 -0500

    Part 2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+	has_many :comments
+	
 	validates :content, :presence => true, :length => { :minimum => 20 }
   validates :title, :presence => true
   validates :name, :presence => true
