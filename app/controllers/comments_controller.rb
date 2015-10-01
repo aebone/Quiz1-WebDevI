52e0ebf080d282a1cae7ddf88c9c44bdedd8e28d
Aline Ebone <ini.ebone@gmail.com>
2015-10-01 13:41:52 -0500

    Some changes.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,11 @@
 class CommentsController < ApplicationController
 	 def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment])
+    @comment = @post.comments.create!(comment_params)
     redirect_to post_path(@post)
+  end
+
+  def comment_params
+  	params.require(:comment).permit(:author, :body)
   end
 end
